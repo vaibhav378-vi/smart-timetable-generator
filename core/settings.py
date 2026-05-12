3d7c09c34b4b05061fbfcddbabdfc7c1094c24b4
Vaibhav Bansal <vaibhavbansal378gmail.com>
2026-05-13 00:16:02 +0530

    fixed all core references

--- core/settings.py
+++ core/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'timetable_generator.urls''
+ROOT_URLCONF = 'core.urls''
 
 TEMPLATES = [
     {
@@ -66,7 +66,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'timetable_generator.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
